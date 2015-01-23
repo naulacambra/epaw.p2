006742c3231a5e3b2dce5459517062bd0a1f789d
Arnau Lacambra <naulacambra@gmail.com>
2015-01-23 15:12:22 +0100

    Set user attributes

--- src/models/BeanUser.java
+++ src/models/BeanUser.java
@@ -2,45 +2,63 @@ package models;
 
 public class BeanUser {
 
-	private String user = "";
+	private String fullName = "";
+	private String username = "";
 	private String mail = "";
-	private int[] error = {0,0}; 
-	
+	private String password = "";
+	private int[] error = { 0, 0 };
+
 	/* Getters */
-	public String getUser(){
-		return user;
+	public String getFullName() {
+		return fullName;
 	}
-	
+
+	public String getUser() {
+		return username;
+	}
+
 	public String getMail() {
 		return mail;
 	}
-	
+
+	public String getPassword() {
+		return password;
+	}
+
 	public int[] getError() {
 		return error;
 	}
-	
-	/*Setters*/
-	public void setUser(String user){
-		
-		//Consultar si existeix un usuari igual a la BD
+
+	/* Setters */
+	public void setFullName(String fullName) {
+
+		// Consultar si existeix un usuari igual a la BD
 		error[0] = 1;
-		
+
 	}
-	
-	public void setMail(String mail){
+
+	public void setUsername(String username) {
+
+		// Consultar si existeix un usuari igual a la BD
+		error[0] = 1;
+
+	}
+
+	public void setMail(String mail) {
 		this.mail = mail;
 	}
 	
-	/* Logic Functions */
-	
-	
-	
-	public boolean isComplete() {
-	    return(hasValue(getUser()) &&
-	           hasValue(getMail()) );
+	public void setPassword(String password) {
+		this.password = password;
 	}
-	
+
+	/* Logic Functions */
+
+	public boolean isComplete() {
+		return (hasValue(getUser()) && hasValue(getMail()));
+	}
+
 	private boolean hasValue(String val) {
-		return((val != null) && (!val.equals("")));
+		return ((val != null) && (!val.equals("")));
 	}
 }
